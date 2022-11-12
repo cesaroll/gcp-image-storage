f0486a4d687ffc3fa5942abb0a3818e7d081e5f0
Cesar Lopez <cesarlerdeut@gmail.com>
2022-11-12 12:48:09 +0100

    Itinial Commit

---
+++ src/main/java/io/ces/gdrive/images/gcpimagestorage/GcpImageStorageApplication.java
@@ -0,0 +1,13 @@
+package io.ces.gdrive.images.gcpimagestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpImageStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpImageStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ces/gdrive/images/gcpimagestorage/GcpImageStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ces.gdrive.images.gcpimagestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpImageStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
